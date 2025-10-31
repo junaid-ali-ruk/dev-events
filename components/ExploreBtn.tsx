fdadec864d3b272a7ce40095358c70ec370aee55
junaid-ali-ruk <junaid2025.ali@gmail.com>
2025-10-31 10:08:09 +0500

    first commit

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 export default function ExploreBtn() {
   return (
-    <button>Explore Now</button>
+    <button type="button" id="explore-btn" className="mt-7 mx-auto">
+     <a href="#events">
+      Explore Now 
+      <Image src={"/icons/arrow-down.svg"} alt="arrow-down" height={24} width={24}/>
+     </a>
+    </button>
   );
 }
